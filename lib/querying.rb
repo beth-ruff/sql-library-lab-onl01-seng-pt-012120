38e7e48490a6e08a66712da3b6e17a979d199acb
Beth Ruff <beth.ruff2@gmail.com>
2020-03-24 19:37:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,5 +45,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   JOIN character_books 
   ON character_books.character_id = characters.id 
+  ORDER BY COUNT(*)
   "
 end
